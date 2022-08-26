2bdaa28bad994ed396301d885271f6f3574f3ea0
kTrzcinskii <kt.trzcinski@gmail.com>
2022-08-27 01:01:41 +0200

    index page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -7,11 +8,35 @@ const Home: NextPage = () => {
       <Head>
         <title>Chat app</title>
         <meta name='description' content='Chat app by Kacper Trzciński' />
-        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className='container mx-auto flex flex-col items-center justify-center min-h-screen p-4'>
-        <h1>hello</h1>
+      <main className='w-full flex flex-col items-center justify-center min-h-screen p-4'>
+        <h1 className='text-6xl font-semibold text-my-blue-light'>Chat App</h1>
+        <div className='flex flex-row space-x-4 pt-12'>
+          <Link passHref href='/login'>
+            <button className='btn my-bg-blue text-xl min-w-[98px]'>
+              Login
+            </button>
+          </Link>
+          <Link passHref href='/register'>
+            <button className='btn my-bg-blue text-xl min-w-[98px]'>
+              Register
+            </button>
+          </Link>
+        </div>
+        <div className='absolute bottom-8 pt-8'>
+          <h3 className='text-my-gray-light text-xl'>
+            Created by{" "}
+            <a
+              href='https://github.com/kTrzcinskii'
+              className='text-my-cyan hover:underline'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Kacper Trzciński
+            </a>
+          </h3>
+        </div>
       </main>
     </>
   );
