4e74085e65c8463cb6e901d85c17520684f10a6c
kTrzcinskii <kt.trzcinski@gmail.com>
2022-08-27 13:21:49 +0200

    react query setup

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClinet = new QueryClient();
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClinet}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default MyApp;
