dae53adde7b6f3b519f757982f7c154c7e686c0b
kTrzcinskii <kt.trzcinski@gmail.com>
2022-08-27 13:17:10 +0200

    react hook form props on input

--- client/src/components/home/Input.tsx
+++ client/src/components/home/Input.tsx
@@ -3,38 +3,48 @@ import {
   InputHTMLAttributes,
   ReactNode,
   SetStateAction,
-  useState,
 } from "react";
+import { FieldErrorsImpl, UseFormRegister } from "react-hook-form";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   icon?: ReactNode;
   setShowPassword?: Dispatch<SetStateAction<boolean>>;
+  register: UseFormRegister<any>;
+  registerName: string;
+  errors: FieldErrorsImpl<any>;
 }
 
 const Input: React.FC<InputProps> = ({
   setShowPassword = () => console.log(),
+  register,
+  registerName,
+  errors,
   icon,
   id,
   label,
   ...props
 }) => {
-  const [isInputFocused, setIsInputFocused] = useState(false);
-
-  const labelTextColor = isInputFocused ? "text-my-cyan-light" : "text-white";
+  const isError = errors[registerName] ? true : false;
+  const errorMessage = String(errors[registerName]?.message) ?? "Error";
 
   return (
-    <div className='space-y-2'>
-      <label className={`ml-1 text-lg ${labelTextColor}`} htmlFor={id}>
+    <div
+      className={`space-y-2 text-white focus-within:text-my-cyan-light ${
+        isError && "!text-red-400"
+      }`}
+    >
+      <label className='ml-1 text-lg text-inherit' htmlFor={id}>
         {label}
       </label>
       <div className='relative'>
         <input
           id={id}
           {...props}
-          className='bg-inherit appearance-none border-2 border-white focus:border-my-cyan-light rounded-lg w-full py-2 px-3 text-white leading-tight focus:outline-none transition-all duration-100 ease-in-out'
-          onFocus={() => setIsInputFocused(true)}
-          onBlur={() => setIsInputFocused(false)}
+          className={`bg-inherit appearance-none border-2 border-white focus:border-my-cyan-light rounded-lg w-full py-2 px-3 text-white leading-tight focus:outline-none transition-all duration-100 ease-in-out ${
+            isError && "!border-red-400"
+          }`}
+          {...register(registerName)}
         />
         <div className='absolute right-0 top-0 pr-2 h-full flex items-center'>
           <button
@@ -45,6 +55,7 @@ const Input: React.FC<InputProps> = ({
           </button>
         </div>
       </div>
+      {isError && <p className='ml-1'>{errorMessage}</p>}
     </div>
   );
 };
