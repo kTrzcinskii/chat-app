821cbb16b6a3cccf201312de7b8fa4d9b33a1095
kTrzcinskii <kt.trzcinski@gmail.com>
2022-08-28 21:50:35 +0200

    store access token in local storage

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -2,4 +2,12 @@ import axios from "axios";
 
 const axiosInstance = axios.create({ withCredentials: true });
 
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (config.headers) {
+    config.headers.Authorization = token ? `Bearer ${token}` : "";
+  }
+  return config;
+});
+
 export default axiosInstance;
