204b8b570677b330c151631ed897613da851f609
kTrzcinskii <kt.trzcinski@gmail.com>
2022-09-02 17:42:18 +0200

    center option

--- client/src/components/utils/Spinner.tsx
+++ client/src/components/utils/Spinner.tsx
@@ -1,8 +1,19 @@
 import { ImSpinner5 } from "react-icons/im";
 
-const Spinner: React.FC = () => {
+interface SpinnerProps {
+  centered?: boolean;
+}
+
+const Spinner: React.FC<SpinnerProps> = ({ centered = false }) => {
+  const centeredClasses =
+    "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2";
+
   return (
-    <div className='flex w-full justify-center items-center text-white bg-inherit absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+    <div
+      className={`flex w-full justify-center items-center text-white bg-inherit ${
+        centered && centeredClasses
+      }`}
+    >
       <ImSpinner5 className='animate-spin' size={60} />
     </div>
   );
