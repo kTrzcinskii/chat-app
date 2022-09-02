e4657ede1cb3331b0563b0133e116f7d21da3874
kTrzcinskii <kt.trzcinski@gmail.com>
2022-09-02 23:37:34 +0200

    chatroom container init (much to be done)

---
+++ client/src/components/chatroom/ChatroomsContainer.tsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import useGetAllChatroom from "../../hooks/query/useGetAllChatrooms";
+import ErrorMsg from "../utils/ErrorMsg";
+import Spinner from "../utils/Spinner";
+import ChatroomCard from "./ChatroomCard";
+
+const ChatroomsContainer: React.FC = () => {
+  const { data, fetchNextPage, isFetching, isError, error } =
+    useGetAllChatroom(1);
+
+  const router = useRouter();
+
+  if (isError && axios.isAxiosError(error)) {
+    if (error.response?.status === 401) {
+      router.push("/unauthorized");
+    }
+  }
+
+  return (
+    <div className='bg-my-dark-light w-full h-full relative'>
+      <h1 className='w-full text-center text-white mt-10 text-3xl lg:text-4xl mb-10'>
+        My chatrooms
+      </h1>
+      {data?.pages.map((page, index) => {
+        return (
+          <Fragment key={index}>
+            {page.chatrooms.map((chatroom) => {
+              return (
+                <Fragment key={chatroom.id}>
+                  <ChatroomCard {...chatroom} />
+                </Fragment>
+              );
+            })}
+          </Fragment>
+        );
+      })}
+      {isFetching && <Spinner centered={!data} />}
+      {isError && <ErrorMsg message={error?.message} />}
+      <button onClick={() => fetchNextPage()}>fetch</button>
+    </div>
+  );
+};
+
+export default ChatroomsContainer;
