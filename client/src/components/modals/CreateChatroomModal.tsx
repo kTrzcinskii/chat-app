b214f34f93a3a8430508c7fdbee465b395af0b68
kTrzcinskii <kt.trzcinski@gmail.com>
2022-09-06 20:45:56 +0200

    added radio input to create chatroom form

--- client/src/components/modals/CreateChatroomModal.tsx
+++ client/src/components/modals/CreateChatroomModal.tsx
@@ -1,4 +1,4 @@
-import Input from "../home/Input";
+import Input from "../utils/Input";
 import ModalWrapper from "./ModalWrapper";
 import { useForm } from "react-hook-form";
 import {
@@ -6,6 +6,7 @@ import {
   ICreateChatroomSchema,
 } from "../../utils/schemas/CreateChatroomSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import RadioInput from "../utils/RadioInput";
 
 interface CreateChatroomModalInterface {
   isVisible: boolean;
@@ -25,13 +26,19 @@ const CreateChatroomModal: React.FC<CreateChatroomModalInterface> = ({
     resolver: zodResolver(CreateChatroomSchema),
   });
 
+  console.log(errors);
+
+  const onSubmit = (values: ICreateChatroomSchema) => {
+    console.log(values);
+  };
+
   return (
     <ModalWrapper isVisible={isVisible} closeModal={closeModal}>
       <div>
-        <h1 className='w-full text-center text-3xl text-my-cyan-light mb-8'>
+        <h1 className='w-full text-center text-3xl text-my-blue-dark mb-8'>
           Create New Chatroom
         </h1>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className='flex flex-col w-full space-y-3 max-w-[350px] mx-auto'>
             <Input
               label='Name'
@@ -40,8 +47,35 @@ const CreateChatroomModal: React.FC<CreateChatroomModalInterface> = ({
               register={register}
               registerName='name'
               errors={errors}
-              lightMode={true}
+              lightMode
             />
+            <div>
+              <h3 className='text-lg ml-1 text-zinc-600 mb-2'>
+                Choose privacy:
+              </h3>
+              <div className='flex w-full justify-around'>
+                <RadioInput
+                  label='Public'
+                  register={register}
+                  registerName='privacy'
+                  id='public-radio'
+                  checked
+                  value='PUBLIC'
+                />
+                <RadioInput
+                  label='Private'
+                  register={register}
+                  registerName='privacy'
+                  id='private-radio'
+                  value='PRIVATE'
+                />
+              </div>
+            </div>
+            <div className='w-full flex justify-center items-center pt-5'>
+              <button className='btn my-bg-blue w-[150px]' type='submit'>
+                Create
+              </button>
+            </div>
           </div>
         </form>
       </div>
